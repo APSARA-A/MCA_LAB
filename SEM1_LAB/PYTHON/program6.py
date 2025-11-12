ffe508931f9b9c968b1c366aa8d87c22e06ac71f
APSARA-A <apsarasreemangalam@gmail.com>
2025-11-12 22:01:53 +0530

    compare two lists

---
+++ SEM1_LAB/PYTHON/program6.py
@@ -0,0 +1,6 @@
+list1=input("Enter the first list:").split(",")
+list2=input("enter the second list").split(",")
+if list1==list2:
+    print("two lists are equal")
+else:
+    print("lists are not equal")
