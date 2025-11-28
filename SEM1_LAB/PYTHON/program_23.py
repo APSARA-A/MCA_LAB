1552c962f9e80b46e3a5dbc3cc1c7904fcf2cc55
APSARA-A <apsarasreemangalam@gmail.com>
2025-11-28 22:15:04 +0530

    program using lambda function

---
+++ SEM1_LAB/PYTHON/program_23.py
@@ -0,0 +1,6 @@
+square_area=lambda side: side*side
+rectangle_area=lambda length,breadth:length+breadth
+triangle_area=lambda base,height:0.5*base*height
+print("Area of square:",square_area(5))
+print("Area of rectangle:",rectangle_area(10,4))
+print("Area of triangle:",triangle_area(6,8))
