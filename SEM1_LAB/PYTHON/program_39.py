380ba4ec987a91947f226d19431c91fdf56bf3fd
APSARA-A <apsarasreemangalam@gmail.com>
2026-03-31 07:27:51 +0530

    print diagonal elements of an array

---
+++ SEM1_LAB/PYTHON/program_39.py
@@ -0,0 +1,8 @@
+import numpy as np 
+matrix=np.array([[10,20,30],
+                 [40,50,50],
+                 [70,80,90]])
+
+print("Matrix:")
+print(matrix)
+print("Diagonal elements:",np.diagonal(matrix))
