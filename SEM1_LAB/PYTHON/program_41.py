973c4f8abc2878b38a78867d40ff22b7764bbad9
APSARA-A <apsarasreemangalam@gmail.com>
2026-03-31 07:26:16 +0530

    Arithmetic operations on array

---
+++ SEM1_LAB/PYTHON/program_41.py
@@ -0,0 +1,11 @@
+import numpy as np 
+arr1=np.array([10,20,30,40,50])
+arr2=np.array([60,70,80,90,100])
+
+print("array1:",arr1)
+print("array2:",arr2)
+
+print("addition:",arr1+arr2)
+print("subtraction:",arr2-arr1)
+print("multiplication:", arr1*arr2)
+print("Division:",arr2/arr1)
